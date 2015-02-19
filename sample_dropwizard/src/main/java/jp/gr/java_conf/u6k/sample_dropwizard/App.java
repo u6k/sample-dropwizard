35bd393156fa53810197e99e62a0091f2fbc65e4
Naono Yuichi <u6k.apps@gmail.com>
2015-02-19 16:18:10 +0900

    Mavenプロジェクトを作成しました。

---
+++ sample_dropwizard/src/main/java/jp/gr/java_conf/u6k/sample_dropwizard/App.java
@@ -0,0 +1,13 @@
+package jp.gr.java_conf.u6k.sample_dropwizard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
